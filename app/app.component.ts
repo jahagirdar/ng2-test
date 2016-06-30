581338a2b15fc36f53d08f7f5221915ab552de7a
Vijayvithal <jvs@dyumnin.com>
2016-06-30 08:29:07 +0530

    Working Example with  Chart.js and bootstrap

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,12 @@
 import {Component} from '@angular/core';
+import {CHART_DIRECTIVES} from 'ng2-charts/ng2-charts';
+import {AlertComponent} from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  directives: [CHART_DIRECTIVES],
+  templateUrl:'app/app.component.html',
+  styles:[' .chart {display: block; width: 100%;} ']
 })
-export class AppComponent { }
+export class AppComponent {
+}
